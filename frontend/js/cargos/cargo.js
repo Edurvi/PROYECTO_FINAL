0db621a16290134f24f1054a729b0dfb3a20636b
Edgar Uriel Villavicencio Medina <86541219+Edurvi@users.noreply.github.com>
2022-03-29 11:38:40 -0500

    Delete frontend directory

--- frontend/js/cargos/cargo.js
+++
@@ -1,6 +0,0 @@
-import {Cargo} from './componentes.js'
-//instaciamos cargo
-const serCargo = new Cargo();
-const d = document
-const $formCargo=d.getElementById("form-cargo")
-d.addEventListener("DOMContentLoaded",serCargo.obtenerCargos())
--- frontend/js/departamento/departamento.js
+++
@@ -1,6 +0,0 @@
-import { Departamento } from "./componentesDe.js";
-//instaciamos 
-const serDepartamento = new Departamento();
-const d = document;
-const $formDepartamento = d.getElementById("form-Departamento");
-d.addEventListener("DOMContentLoaded", serDepartamento.obtenerDepartamentos());
