edda8b2e9a39637fe0d94ec3a41bc916b8422d8d
Edgar Uriel Villavicencio Medina <86541219+Edurvi@users.noreply.github.com>
2022-03-29 11:38:50 -0500

    Delete back directory

--- back/dist/controllers/principal.controller.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.menuPrincipal = void 0;
-function menuPrincipal(req, res) {
-    return res.json("Pagina Principal");
-}
-exports.menuPrincipal = menuPrincipal;
--- back/src/interface/cargo.ts
+++
@@ -1,7 +0,0 @@
-// la varaibles que van aqui se crean en base al modelo llamado cargo
-export interface Icargo{
-    id?:number;    // el ? significa que el campo es opcional
-    descripcion:string;
-    estado:number;
-    created_at:Date;
-}
